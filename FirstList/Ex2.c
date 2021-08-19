12fa57f934308272cf93d6973e965dd987d55e13
Otávio Fernandes dos Santos <otaviofs01@gmail.com>
2021-08-19 13:05:06 -0300

    Update Ex2.c

--- FirstList/Ex2.c
+++ FirstList/Ex2.c
@@ -5,13 +5,15 @@
 int menu(void);
 char* add(char *names);
 char* remov(char *names);
-void show(char *names);
+void show(char *names, int sizeName);
+
+int sizeName = 0;
  
 int main(){
     int choice;
     char *names;
 
-    names = (char *)malloc(1);
+    names = (char *)calloc(1, 1);
     if (!names){
         printf("Could not allocate required memory!\n");
         return -1;
@@ -22,12 +24,13 @@ int main(){
         switch (choice){
             case 1:
                 names = add(names);
+                sizeName++;
                 break;
             case 2:
                 names = remov(names);
                 break;
             case 3:
-                show(names);
+                show(names, sizeName);
                 break;
             case 4:
                 free(names);
@@ -38,27 +41,37 @@ int main(){
     return 0;
 }
 
-void show(char *names){
-    if(!strlen(names)){
-        printf("\nNo names!\n");
-    }else{
-        printf("\n-All names:\n%s\n", names);
+void show(char *names, int sizeName){
+    if(sizeName == 0){
+        printf("\nNo names yet!\n");
+        return;
     }
+    
+    printf("\n-All names:\n%s\n", names);
 }
 
 char* remov(char *names){
     char buffer[10];
-    int i = 0;
+    int i = 0, size;
     printf("\n-Remove name: "); scanf("%s", buffer);
 
     char *pch = strstr(names, buffer);
+    size = strlen(names);
 
     if(pch){
 
         for(i = 0; i < strlen(buffer)+1; i++){
-            strcpy(pch, (pch+1));
+            memmove(pch, (pch+1), strlen(pch+1) + 1);
         }
-        names = (char *)realloc(names, strlen(names)-i);
+
+        if(size == i){
+            names = (char *)realloc(names, size-i+2);
+            sizeName--;
+            return names;
+        }
+        names = (char *)realloc(names, size-i+1);
+
+        sizeName--;
 
     }else{
         printf("\nName not found!\n");
@@ -72,7 +85,7 @@ char* add(char *names){
     printf("\n-Add name: "); scanf("%s", buffer);
     strcat(buffer, " ");
 
-    names = (char *)realloc(names, strlen(names)+strlen(buffer));
+    names = (char *)realloc(names, strlen(names)+strlen(buffer)+1);
     strcat(names, buffer);
 
     return names;
