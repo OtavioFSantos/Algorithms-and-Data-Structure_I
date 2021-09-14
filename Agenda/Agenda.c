ea86f271ccf20a40c706ec93b5bf7da47df39a19
Otávio Fernandes dos Santos <otaviofs01@gmail.com>
2021-09-14 20:44:02 -0300

    Update Agenda.c

--- Agenda/Agenda.c
+++ Agenda/Agenda.c
@@ -3,23 +3,30 @@
 #include <string.h>
 
 void menu(void *pBuffer);
-void add(void *pBuffer, void *pSentinel);
-// void *remov(void *pBuffer);
-// void show(void *pBuffer);
-// void search(void *pBuffer);
+void add(void *pBuffer, void *pSentinel, void *aux);
+void showall(void *pBuffer, void *pSentinel, void *aux);
 
-//personsize = name[20] + age + tel + nextAddress
-#define PERSON_SIZE (sizeof(char)*20 + sizeof(int) + sizeof(long int) + sizeof(void **))
- 
+//personsize = name[20] + age + tel + previousAddress + nextAddress
+#define PERSON_SIZE (sizeof(char)*20+sizeof(int)+sizeof(long int)+sizeof(void **)*2)
+#define NEXT (sizeof(char)*20+sizeof(int)+sizeof(long int)+sizeof(void **))
+#define PREV (sizeof(char)*20+sizeof(int)+sizeof(long int))
+#define PTOP (sizeof(int))
+#define FIRST (sizeof(int)+sizeof(void **))
+#define LAST (sizeof(int))
+
+
 int main(){
     void *pBuffer, *pSentinel;
 
-    //pBuffer = choice + i
-    pBuffer = (void *)calloc(2, sizeof(int));
-    //pSentinel = size + sentinel(first address)
-    pSentinel = (void *)calloc(1, (sizeof(int) + sizeof(void *)));
+    void *aux = *(void **)calloc(1, PERSON_SIZE);
 
-    if (!pBuffer || !pSentinel){
+    //pSentinel = size + last address + first address
+    pSentinel = (void *)calloc(1, (sizeof(int) + sizeof(void **)*2));
+
+    //pBuffer = choice + pointer to pointer
+    pBuffer = (void *)calloc(1, (sizeof(int) + sizeof(void **)));
+
+    if(!pBuffer || !pSentinel){
         printf("Could not allocate required memory!\n");
         return -1;
     }
@@ -27,11 +34,19 @@ int main(){
     //size = 0
     *(int *)pSentinel = 0;
 
+    //last address = NULL
+    *(void **)(pSentinel+sizeof(int)) = NULL;
+
+    //first address = NULL
+    *(void **)(pSentinel+FIRST) = NULL;
+
+    *(void **)(pBuffer+PTOP) = NULL;
+
     for(;;){
         menu(pBuffer);
         switch(*(int *)pBuffer){
             case 1:
-                add(pBuffer, pSentinel);
+                add(pBuffer, pSentinel, aux);
                 break;
             case 2:
                 // pBuffer = remov(pBuffer);
@@ -41,11 +56,12 @@ int main(){
                 //search(pBuffer);
                 break;
             case 4:
-                //show(pBuffer);
+                showall(pBuffer, pSentinel, aux);
                 break;
             case 5:
                 free(pBuffer);
                 free(pSentinel);
+                free(aux);
                 exit(0);
                 break;
 		}
@@ -53,152 +69,120 @@ int main(){
     return 0;
 }
 
-/*
-void *remov(void *pBuffer){
-    char name[10], nameaux[10];
-    int person, i, c, k, j;
-
-    if(size == 0){
-        printf("\nNo data yet!\n");
-        return pBuffer;
-    }
-
-    person = sizeof(int)*2+sizeof(char)*10;
-
-    
-    printf("Name to remove: "); scanf("%s", name);
 
 
-    for(i = size, c = 1; c < size+1; i--, c++){
+void add(void *pBuffer, void *pSentinel, void *aux){
+    void *aux2;
 
-        for(k = 0; k < strlen(name)+1; k++){
-            nameaux[k] = *(char *)(pBuffer + k + person*(size-i) + sizeof(int));
-        }
-
-        if(strcmp(nameaux, name) == 0){
-
-            if(c != size){
-
-                for(j = 0; j < 10; j++){
-                    *(char *)(pBuffer + j + person*(size-i) + sizeof(int)) = *(char *)(pBuffer + j + person*(size-i+1) + sizeof(int));
-                }
-                *(int *)(pBuffer+sizeof(char)*10+person*(size-i) + sizeof(int)) = *(int *)(pBuffer+sizeof(char)*10+person*(size-i+1) + sizeof(int));
-                *(int *)(pBuffer+sizeof(char)*10+sizeof(int)+person*(size-i) + sizeof(int)) = *(int *)(pBuffer+sizeof(char)*10+sizeof(int)+person*(size-i+1) + sizeof(int));
-
-
-                for(i = i - 1; c < size-1; i--, c++){
-                    for(j = 0; j < 10; j++){
-                        *(char *)(pBuffer + j + person*(size-i) + sizeof(int)) = *(char *)(pBuffer + j + person*(size-i+1) + sizeof(int));
-                    }
-                    *(int *)(pBuffer+sizeof(char)*10+person*(size-i) + sizeof(int)) = *(int *)(pBuffer+sizeof(char)*10+person*(size-i+1) + sizeof(int));
-                    *(int *)(pBuffer+sizeof(char)*10+sizeof(int)+person*(size-i) + sizeof(int)) = *(int *)(pBuffer+sizeof(char)*10+sizeof(int)+person*(size-i+1) + sizeof(int));
-                }
-
-            }
-
-            pBuffer = (char *)realloc(pBuffer, person*(size-1)+sizeof(int));
-            if (!pBuffer){
-                printf("Could not allocate required memory!\n");
-                exit(1);
-            }
-            size--;
-            return pBuffer;
-        }
-    }
-
-    return pBuffer;
-}*/
-
-/*
-void search(void *pBuffer){
-    char name[10], nameaux[10];
-    int person, i = 0, c = 0, k = 0;
-
-    person = sizeof(int)*2+sizeof(char)*10;
-
-    if(size == 0){
-        printf("\nNo data yet!\n");
-        return;
-    }
-    
-    printf("Name to search: "); scanf("%s", name);
-
-
-    pBuffer = pBuffer + sizeof(int); 
-
-    for(i = size, c = 0; c < size; i--, c++){
-
-        for(k = 0; k < strlen(name)+1; k++){
-            nameaux[k] = *(char *)(pBuffer + k + person*(size-i));
-        }
-        if(strcmp(nameaux, name) == 0){
-            printf("\nPerson[%d]: \tName: ", c);
-            for (k = 0; *(char *)(pBuffer + k + person*(size-i)) >= 'a' && *(char *)(pBuffer + k + person*(size-i)) <= 'z'; k++){
-                printf("%c", *(char *)(pBuffer + k + person*(size-i)));
-            }
-            printf("\n\t\tAge: %d\n\t\tTel: %d\n\n", 
-            *(int *)(pBuffer+sizeof(char)*10+person*(size-i)), *(int *)(pBuffer+sizeof(char)*10+sizeof(int)+person*(size-i)));
-        }
-    }
-}*/
-
-
-void add(void *pBuffer, void *pSentinel){
-
-    //*(int *)(pPeople+sizeof(int)) = (void *)calloc(1, PERSON_SIZE);
     void *pPerson = (void *)malloc(PERSON_SIZE);
     if (!pPerson){
         printf("Could not allocate required memory!\n");
-        return -1;
+        return;
+    }
+    printf("Address: %p", pPerson);
+    printf("\n-Add name: "); scanf("%19[^\n]c", (char *)pPerson); getchar();
+    printf("-Add age: "); scanf("%d", &*(int *)(pPerson+sizeof(char)*20));
+    printf("-Add tel: "); scanf("%ld", &*(long int *)(pPerson+sizeof(char)*20+sizeof(int)));
+
+    //nextAdress = NULL;
+    *(void **)(pPerson+NEXT) = NULL;
+    //previousAddress = NULL;
+    *(void **)(pPerson+PREV) = NULL;
+
+    printf("Name: %s, age: %d, tel: %ld, previous: %p, next: %p\n", (char *)pPerson, *(int *)(pPerson+sizeof(char)*20), *(long int *)(pPerson+sizeof(char)*20+sizeof(int)),*(void **)(pPerson+PREV), *(void **)(pPerson+NEXT));
+
+
+    if(*(int *)pSentinel == 0){
+        //pSentinel receive first address
+        *(void **)(pSentinel+LAST) = pPerson;
+        *(void **)(pSentinel+FIRST) = pPerson;
+        printf("\nFirst address: %p\n", *(void **)(pSentinel+FIRST));
+
+        //size++
+        *(int *)pSentinel += 1;
+        return;
     }
 
-    printf("\n-Add name: "); scanf("%19[^\n]c", pPerson); getchar();
-    printf("-Add age: "); scanf("%ld", &*(int *)(pPerson+sizeof(char)*20));
-    printf("-Add tel: "); scanf("%d", &*(long int *)(pPerson+sizeof(char)*20+sizeof(int)));
-    *(void **)(pPerson+sizeof(char)*20+sizeof(int)+ sizeof(long int)) = NULL;
+    aux = *(void **)(pSentinel+FIRST);
+    
+    do{
+        if(strcmp((char *)pPerson, (char *)aux) < 0){
+            
+            //ed = de
+            *(void **)(pPerson+PREV) = *(void **)(aux+PREV);
+            //ed = ot
+            *(void **)(pPerson+NEXT) = aux;
 
-    printf("Name: %s, age: %d, tel: %ld\n", pPerson, *(int *)(pPerson+sizeof(char)*20), *(long int *)(pPerson+sizeof(char)*20+sizeof(int)));
+            if(*(int *)pSentinel > 1 && *(void **)(aux+PREV) != NULL){
+                aux2 = *(void **)(aux+PREV);
+                printf("DENISE: %s\n", aux2);
+                //de = ed
+                *(void **)(aux2+NEXT) = pPerson;
+            }
+            //ot = ed
+            *(void **)(aux+PREV) = pPerson;
+            
+            
+
+            if(*(void **)(pPerson+PREV) == NULL){
+                //pSentinel receive first address
+                *(void **)(pSentinel+FIRST) = pPerson;
+                printf("\nFirst address: %p\n", *(void **)(pSentinel+FIRST));
+            }
+            if(*(void **)(aux+NEXT) == NULL && *(int *)pSentinel == 1){
+                //pSentinel receive last address
+                *(void **)(pSentinel+LAST) = aux;
+                printf("\nLast address: %p\n", *(void **)(pSentinel+LAST));
+            }
+
+            //size++
+            *(int *)pSentinel += 1;
+            //free(aux);
+            printf("\nFirst address: %s\n", *(void **)(pSentinel+FIRST));
+            printf("\nLast address: %s\n", *(void **)(pSentinel+LAST));
+            return;
+        }
+        
+        if(*(void **)(aux+NEXT) == NULL){
+            aux2 = aux;
+        }
+        //receive next address
+        aux = *(void **)(aux+NEXT);
+    }while(aux != NULL);
+
+    printf("OTAVIO: %s\n", aux2);
+
+    //wi = ot
+    *(void **)(pPerson+PREV) = aux2;
+    //ot = wi
+    *(void **)(aux2+NEXT) = pPerson;
+
+    *(void **)(pSentinel+LAST) = pPerson;
+
+    printf("\nFirst address: %s\n", *(void **)(pSentinel+FIRST));
+    printf("\nLast address: %s\n", *(void **)(pSentinel+LAST));
 
 
     //size++
     *(int *)pSentinel += 1;
-
-    if(*(int *)pSentinel == 0){
-        //pSentinel receive first address
-        *(void **)(pSentinel+sizeof(int)) = &pPerson;
-    }else{
-        for(;;){
-
-        }
-    }
+    //free(aux);
+    return;
 }
 
-/*
-void show(void *pBuffer){
-    int person, i = 0, c = 0, k = 0;
+
+void showall(void *pBuffer, void *pSentinel, void *aux){
     
-    person = sizeof(int)*2+sizeof(char)*10;
+    aux = *(void **)(pSentinel+FIRST);
+    do{
+        printf("\n--Current address: %p\n", aux);
+        printf("\tPerson: %s\n", aux);
+        printf("--Previous address: %p\n", *(void **)(aux+PREV));
+        printf("--Next address: %p\n", *(void **)(aux+NEXT));
 
-    if(size == 0){
-        printf("\nNo data yet!\n");
-        return;
-    }
-    printf("Size: %d\n\n", *(int *)pBuffer);
-
-
-    pBuffer = pBuffer + sizeof(int); 
-
-    for(i = size, c = 0; c < size; i--, c++){
-        
-        printf("Person[%d]: \tName: ", c);
-        for(k = 0; *(char *)(pBuffer + k + person*(size-i)) >= 'a' && *(char *)(pBuffer + k + person*(size-i)) <= 'z'; k++){
-            printf("%c", *(char *)(pBuffer + k + person*(size-i)));
-        }
-
-        printf("\n\t\tAge: %d\n\t\tTel: %d\n\n", 
-        *(int *)(pBuffer+sizeof(char)*10+person*(size-i)), *(int *)(pBuffer+sizeof(char)*10+sizeof(int)+person*(size-i)));
-    }
-}*/
+        //receive next address
+        aux = *(void **)(aux+NEXT);
+    }while(aux != NULL);
+}
 
 
 void menu(void *pBuffer){
